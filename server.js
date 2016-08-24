89ae12ff7a4ce99c7e2a077e1f63eb243d778ddc
Hack Reactor Students <accounts@hackreactor.com>
2016-08-24 16:17:08 -0700

    Console log test

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ var port = 4568;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+console.log('YOUNG MONEY GOT THAT SSH AUTHENTICATION, SON!');
+console.log('DO YOU EVEN GANGSTA RAP, BRO?');
