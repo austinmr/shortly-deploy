97d1a86239041584bf8dc3d8d21fde8b33acc148
Hack Reactor Students <accounts@hackreactor.com>
2016-08-24 17:15:28 -0700

    Added completely essential headers

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ app.listen(port);
 console.log('Server now listening on port ' + port);
 console.log('YOUNG MONEY GOT THAT SSH AUTHENTICATION, SON!');
 console.log('DO YOU EVEN GANGSTA RAP, BRO?');
+
+
